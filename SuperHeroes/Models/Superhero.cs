8ba0076bf01c0b5f29d20fe517c59aa97fe09178
unknown <kylee.schreibs2@yahoo.com>
2019-04-27 13:40:19 -0500

    add Superhero model and properties in it

---
+++ SuperHeroes/Models/Superhero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SuperHeroes.Models
+{
+    public class Superhero
+    {
+        [Key]
+        public string name { get; set; }
+        public string alterEgo { get; set; }
+        public string primaryAbility { get; set; }
+        public string secondaryAbility { get; set; }
+        public string catchPhrase { get; set; }
+    }
+}
